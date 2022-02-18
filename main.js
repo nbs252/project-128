1d0021befb4fd153e55e7e608e1078e82bb703b2
nbs252 <87271185+nbs252@users.noreply.github.com>
2022-02-18 16:44:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWristX = 0;
+scoreLeftWristY = 0;
+
 
 function setup(){
 canvas = createCanvas(600, 500);
@@ -26,6 +29,20 @@ song_2 = loadSound("music2.mp3");
 
 function draw(){
 image(video, 0, 0, 600, 500);
+fill("#00000");
+stroke("#00000");
+
+circle(rightWristX, rightWristY, 20);
+
+if(scoreleftWrist > 0.2){
+circle(leftWristX, leftWristY, 20);
+stop("music2.mp3");
+} else if(song_1 == false){
+song.setSong("music.mp3");
+song.setVolume(1);
+song.rate(1);
+document.getElementById("song_1").innerHTML = "Song_1 = " + song_1;
+}
 }
 
 function gotPoses(results){
@@ -38,5 +55,8 @@ console.log("leftWristX "+ leftWristX +"leftWristY"+ leftWristY);
 rightWristX = results[0].pose.rightWrist.x;
 rightWristY = results[0].pose.rightWrist.y;
 console.log("rightWristX"+ rightWristX +"rightWristY"+ rightWristY);
+
+scoreLeftWristX = results[1].pose.leftWrist.x;
+scoreLeftWristY = results[1].pose.leftWrist.y;
 }
 }
